fada8ee2f83ff379a18f5857f4867633b84c0b75
Lautaro Di Sanza <lautarod@gmail.com>
2023-08-14 16:35:48 -0300

    Correcciones

--- backend/src/poap/poap.module.ts
+++ backend/src/poap/poap.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PoapController } from './poap.controller';
 import { PoapService } from './poap.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PoapSchema } from './schemas/poap.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Poap', schema: PoapSchema }])],
   controllers: [PoapController],
   providers: [PoapService]
 })
--- backend/src/quest/quest.module.ts
+++ backend/src/quest/quest.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QuestService } from './quest.service';
 import { QuestController } from './quest.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { QuestSchema } from './schemas/quest.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Quest', schema: QuestSchema }])],
   providers: [QuestService],
   controllers: [QuestController]
 })
