addffcca749fd99d06dfa6ce107b304bab77a1d8
Lautaro Di Sanza <lautarod@gmail.com>
2023-08-15 11:49:34 -0300

    QuestSubscriptions

--- backend/src/quest/dto/createQuestDTO.ts
+++ backend/src/quest/dto/createQuestDTO.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateQuestDTO {
-  @ApiProperty({ example: 'Envio CV' })
+  @ApiProperty({ example: 'Mint NFT' })
   readonly nameQuest: string;
   readonly createdat: Date;
 }
--- backend/src/quest/quest.controller.ts
+++ backend/src/quest/quest.controller.ts
@@ -13,6 +13,7 @@ import {
 } from '@nestjs/common';
 import { QuestService } from './quest.service';
 import { CreateQuestDTO } from './dto/createQuestDTO';
+import { CreateSubscriptionDTO } from './dto/createSubscriptionDTO';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('quest')
@@ -25,7 +26,25 @@ export class QuestController {
     try {
       const quest = await this.questService.createQuest(createQuestDTO);
       return res.status(HttpStatus.OK).json({
-        message: 'POAP created',
+        message: 'Quest created',
+        quest,
+      });
+    } catch (error) {
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error);
+    }
+  }
+
+  @Post('/create/subscription')
+  async createSubscription(
+    @Res() res,
+    @Body() createSubscriptionDTO: CreateSubscriptionDTO,
+  ) {
+    try {
+      const quest = await this.questService.createSubscriptionQuest(
+        createSubscriptionDTO,
+      );
+      return res.status(HttpStatus.OK).json({
+        message: 'Quest subscription created',
         quest,
       });
     } catch (error) {
@@ -37,6 +56,28 @@ export class QuestController {
   async getById(@Res() res, @Param('idQuest') idQuest: string) {
     try {
       const quest = await this.questService.getById(idQuest);
+      if (!quest) throw new NotFoundException('Quest Does not exists');
+      return res.status(HttpStatus.OK).json(quest);
+    } catch (error) {
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error);
+    }
+  }
+
+  @Get()
+  async getQuests(@Res() res) {
+    try {
+      const quests = await this.questService.getQuests();
+      if (!quests) throw new NotFoundException('Quest Does not exists');
+      return res.status(HttpStatus.OK).json(quests);
+    } catch (error) {
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error);
+    }
+  }
+
+  @Get('/getSubscriptionsByIdUser/:idUser')
+  async getSubscriptionsByIdUser(@Res() res, @Param('idUser') idUser: string) {
+    try {
+      const quest = await this.questService.getSubscriptionsByIdUser(idUser);
       if (!quest) throw new NotFoundException('Quest Does not exists');
       return res.status(HttpStatus.OK).json(quest);
     } catch (error) {
