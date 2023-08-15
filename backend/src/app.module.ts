3e597c77fc7d3cb1f523f2fd097f53bd6ddb2e7a
Lautaro Di Sanza <lautarod@gmail.com>
2023-08-15 08:39:58 -0300

    Ajustes variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { DATABASE_CONEXION_STRING } from '../enviroment.json';
 import { PoapModule } from './poap/poap.module';
 import { QuestModule } from './quest/quest.module';
+import 'dotenv/config';
 @Module({
-  imports: [UserModule, MongooseModule.forRoot(DATABASE_CONEXION_STRING), PoapModule, QuestModule],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot(process.env.DATABASE_CONEXION_STRING),
+    PoapModule,
+    QuestModule,
+  ],
   controllers: [],
   providers: [],
 })
